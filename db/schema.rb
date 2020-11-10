b60b0a054c94015a61fca9784192f30f745e2b89
Bradley <bmcguigandev@outlook.com>
2020-11-10 09:10:59 +0000

    update results form for bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_160240) do
+ActiveRecord::Schema.define(version: 2020_11_10_082348) do
 
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.string "code"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -24,6 +30,13 @@ ActiveRecord::Schema.define(version: 2020_11_09_160240) do
     t.integer "duration"
     t.integer "departure_airport_id"
     t.integer "destination_airport_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
